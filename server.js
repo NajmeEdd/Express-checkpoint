14a8281bcacfc69e391a240ac40090e787ec5af4
NajmeEdd <NajmeEdd@users.noreply.github.com>
2020-11-25 15:48:57 +0100

    first commit

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+app.use(express.static(__dirname+'/public'));
+
+const workingTime = (req, res, next) => {
+  let openDay = new Date();
+
+  if (
+    openDay.getHours() > 09 &&
+    openDay.getHours() < 17 &&
+    openDay.getDay() > 0 &&
+    openDay.getDay() < 6
+  ) {
+    next();
+  } else {
+    console.log("Our office is closed !!!");
+    res.send("<h1> please contact us on working time :) ");
+  }
+};
   app.get("/", function (req, res) {
     res.sendFile(__dirname + "/public/home.html");
   });
